86e44511dc93a5f5940f3e6638c1712beb6f472b
manojpunith13 <86561587+manojpunith13@users.noreply.github.com>
2026-04-27 11:28:55 +0530

    Time: 722 ms (7.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    S.user_id,
+    ROUND(
+        ISNULL(
+            AVG(CASE WHEN C.action = 'confirmed' THEN 1.0 ELSE 0 END),
+        0), 
+    2) AS confirmation_rate
+FROM Signups S
+LEFT JOIN Confirmations C
+    ON S.user_id = C.user_id
+GROUP BY S.user_id;
